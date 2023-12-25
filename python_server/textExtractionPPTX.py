d73baffb286e2c6bb1fb670d5797f711014abd1e
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-25 19:39:32 +0500

    Add textExtractionPPTX function for extracting text from PowerPoint slides

---
+++ python_server/textExtractionPPTX.py
@@ -0,0 +1,9 @@
+from langchain.document_loaders import UnstructuredPowerPointLoader
+
+def textExtractionPPTX(path):
+        
+    loader = UnstructuredPowerPointLoader(path)
+    data = loader.load_and_split()
+    extractedText = data[0].page_content
+
+    return extractedText
