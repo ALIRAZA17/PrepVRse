4cb3d1028f8f570ca2ce2b5f9482755340465020
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-25 19:39:12 +0500

    Add textExtractionPDF function to extract text from PDF files

---
+++ python_server/textExtractionPDF.py
@@ -0,0 +1,10 @@
+from langchain.document_loaders import PyPDFLoader
+
+
+def textExtractionPDF(path):
+    
+    loader = PyPDFLoader(path)
+    pages = loader.load_and_split()
+    extractedText = pages[0].page_content
+
+    return extractedText
