376c4e10cda6e8f68e19f10b7f586d512fb90491
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-27 02:09:52 +0500

    Update questionGeneration template

--- python_server/questionGeneration.py
+++ python_server/questionGeneration.py
@@ -5,7 +5,7 @@ from key import OpenApikey
 
 def questionGeneration(extractedText):
 
-    templateQA = """I am making an app to ask related questions from the user based on the content provided. I have extracted text from powerpoint slides given by user and providing it as content below. Now you need to generate 5 questions from the content, which can be asked from the user about his presentation. (ignore any irrelevant data as I have extracted data from the pptx file so there can be some data which doesnt make any sense. So just focus on the data. Also ignore data such as person names etc)".
+    templateQA = """I am making an app where user can upload presentation slides and perform presentation by recording audio. I have extracted text from powerpoint slides given by user and providing it as content below. Now you need to generate 5 questions, which can be asked from the user at the end of his presentation in QA session and these questions should be in such a way to test user's understanding of different things present in the content which he may not have said during his presentation. Cause you know this is important that questions shouldnt be asking the same thing which the user might have already said during presentation. (ignore any irrelevant data as I have extracted data from the pptx file so there can be some data which doesnt make any sense. So just focus on the data. Also ignore data such as person names etc)".
     If no question can be generated from the provided content then just reply in a professional manner.
 
     Content: {query}
