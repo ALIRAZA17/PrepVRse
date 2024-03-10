cee34ad986327daad3ab27c2ead6306929437eca
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-10 17:22:54 +0500

    server changes

--- python_server/questionGeneration.py
+++ python_server/questionGeneration.py
@@ -4,7 +4,6 @@ from key import OpenApikey
 
 
 def questionGeneration(extractedText):
-
     templateQA = """I am making an app where user can upload presentation slides and perform presentation by recording audio. I have extracted text from powerpoint slides given by user and providing it as content below. Now you need to generate 5 questions, which can be asked from the user at the end of his presentation in QA session and these questions should be in such a way to test user's understanding of different things present in the content which he may not have said during his presentation. Cause you know this is important that questions shouldnt be asking the same thing which the user might have already said during presentation. (ignore any irrelevant data as I have extracted data from the pptx file so there can be some data which doesnt make any sense. So just focus on the data. Also ignore data such as person names etc)".
     If no question can be generated from the provided content then just reply in a professional manner.
 
@@ -23,15 +22,14 @@ def questionGeneration(extractedText):
     formattedTemplateQA = promptTemplateQA.format(
             query=contentQA
         )
-
+    
     # initialize the models
     openai = OpenAI(
-        model_name="text-davinci-003",
+        model_name="gpt-3.5-turbo-instruct",
         openai_api_key= OpenApikey,
         temperature=1
     )
 
-    
     return openai(formattedTemplateQA)
 
 
