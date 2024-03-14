8d3d09bc44fcee2f228828a79f2771f1686de238
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-14 22:48:06 +0500

    structural changes

--- lib/screens/vr_session/ui_vr_session.dart
+++ lib/screens/vr_session/ui_vr_session.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:prepvrse/common/constants/styles.dart';
+import 'package:prepvrse/common/resources/widgets/buttons/app_text_button.dart';
 
 class SessionVR extends StatelessWidget {
   const SessionVR({super.key});
@@ -11,8 +13,25 @@ class SessionVR extends StatelessWidget {
         title: Text("VRSession"),
         backgroundColor: Styles.primaryColor,
       ),
-      body: Center(
-        child: Text("Your Session has started. Please use your VR now!"),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Center(
+              child: Text("Your Session has started. Please use your VR now!"),
+            ),
+          ),
+          Positioned(
+            left: 10,
+            bottom: 5,
+            right: 10,
+            child: AppTextButton(
+              text: "Start Session",
+              onTap: () => Get.toNamed('/feedback'),
+              color: Styles.primaryColor,
+              disabled: true,
+            ),
+          )
+        ],
       ),
     );
   }
