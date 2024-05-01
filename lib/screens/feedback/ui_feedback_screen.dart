706eaee6afa15eca964612a96f23672dd41c1c8d
ALIRAZA17 <ialidar2001@gmail.com>
2024-05-01 16:56:03 +0500

    Update InfoCard widget in feedback screen to show additional information in a dialog

--- lib/screens/feedback/ui_feedback_screen.dart
+++ lib/screens/feedback/ui_feedback_screen.dart
@@ -69,13 +69,14 @@ class _FeedBackScreenState extends State<FeedBackScreen> {
 
     String relevanceText = isRelevanceExpanded
         ? _data["relevance"].trim()
-        : (_data["relevance"]?.length ?? 0) > 350
-            ? _data["relevance"].trim().substring(0, 350) + "..."
+        : (_data["relevance"]?.length ?? 0) > 340
+            ? _data["relevance"].trim().substring(0, 340) + "..."
             : _data["relevance"]?.trim() ?? "";
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Session Feedback'),
+        title: Text('Session Feedback', style: TextStyle(color: Colors.white)),
+        backgroundColor: Theme.of(context).primaryColor,
       ),
       body: isLoading
           ? Center(
@@ -91,142 +92,170 @@ class _FeedBackScreenState extends State<FeedBackScreen> {
               ),
             )
           : SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
+              child: Container(
+                color: Color.fromRGBO(5, 38, 57, 1.000),
                 child: Column(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        InfoCard(
-                          title: 'Sentiment Score',
-                          value: _data["sentiment_score"].toString(),
-                          color: Colors.blue,
-                        ),
-                        InfoCard(
-                          title: 'Speech Rate',
-                          value: _data["speech_rate"].toStringAsFixed(2),
-                          color: Colors.green,
-                        ),
-                      ],
+                    SizedBox(
+                      height: 20,
                     ),
-                    SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        InfoCard(
-                          title: 'Sentiment Class',
-                          value: _data.isNotEmpty
-                              ? _data["sentiment_class"].toString()
-                              : 'N/A',
-                          color: Colors.orange,
-                        ),
-                        InfoCard(
-                          title: 'Vocab Difficulty',
-                          value: _data.isNotEmpty
-                              ? _data["difficulty_class"].toString()
-                              : 'N/A',
-                          color: Colors.red,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        InfoCard(
-                          title: 'Pitch',
-                          value: _data.isNotEmpty
-                              ? _data["average_pitch"].toStringAsFixed(2)
-                              : 'N/A',
-                          color: const Color.fromARGB(255, 234, 84, 134),
-                        ),
-                        InfoCard(
-                          title: 'Grade Level',
-                          value: _data.isNotEmpty
-                              ? _data["grade_level"].toStringAsFixed(2)
-                              : 'N/A',
-                          color: Color.fromARGB(255, 176, 174, 42),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 12),
-                    if (_data.isNotEmpty)
-                      Card(
-                        color: const Color.fromARGB(255, 55, 39, 176),
-                        child: Container(
-                          padding: EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              Text(
-                                'Relevance between Slide Content & Audio',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                              InfoCard(
+                                title: 'Sentiment Score',
+                                value: _data["sentiment_score"].toString(),
+                                dialogInfo:
+                                    "Sentiment Score is a numerical value that represents the sentiment of the audio. The value ranges from -1 to 1. A value of 1 indicates a positive sentiment, a value of -1 indicates a negative sentiment, and a value of 0 indicates a neutral sentiment.",
                               ),
-                              SizedBox(height: 8),
-                              Text(
-                                relevanceText,
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                ),
+                              InfoCard(
+                                title: 'Speech Rate',
+                                value: _data["speech_rate"].toStringAsFixed(2),
+                                dialogInfo:
+                                    "Speech Rate is the speed at which a person speaks. It is measured in words per minute (WPM).",
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  setState(() {
-                                    isRelevanceExpanded = !isRelevanceExpanded;
-                                  });
-                                },
-                                child: Text(
-                                  isRelevanceExpanded ? "See Less" : "See More",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              )
                             ],
                           ),
-                        ),
-                      ),
-                    SizedBox(height: 12),
-                    if (_data.isNotEmpty)
-                      Card(
-                        color: Colors.purple,
-                        child: Container(
-                          padding: EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          SizedBox(height: 12),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              Text(
-                                'Extracted Text from Audio',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                              InfoCard(
+                                title: 'Sentiment Class',
+                                value: _data.isNotEmpty
+                                    ? _data["sentiment_class"].toString()
+                                    : 'N/A',
+                                dialogInfo:
+                                    "Sentiment Class is a classification of the sentiment score. It can be either Positive, Negative, or Neutral.",
                               ),
-                              SizedBox(height: 8),
-                              Text(
-                                displayText,
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                ),
+                              InfoCard(
+                                title: 'Vocab Difficulty',
+                                value: _data.isNotEmpty
+                                    ? _data["difficulty_class"].toString()
+                                    : 'N/A',
+                                dialogInfo:
+                                    "Vocabulary Difficulty is a classification of the vocabulary used in the audio. It can be either Easy, Medium, or Hard.",
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  setState(() {
-                                    isTextExpanded =
-                                        !isTextExpanded;
-                                  });
-                                },
-                                child: Text(
-                                  isTextExpanded ? "See Less" : "See More",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              )
                             ],
                           ),
-                        ),
+                          SizedBox(height: 12),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              InfoCard(
+                                title: 'Pitch',
+                                value: _data.isNotEmpty
+                                    ? _data["average_pitch"].toStringAsFixed(2)
+                                    : 'N/A',
+                                dialogInfo:
+                                    "Pitch is the perceived frequency of a sound. It is measured in Hertz (Hz).",
+                              ),
+                              InfoCard(
+                                title: 'Grade Level',
+                                value: _data.isNotEmpty
+                                    ? _data["grade_level"].toStringAsFixed(2)
+                                    : 'N/A',
+                                dialogInfo:
+                                    "Grade Level is a classification of the vocabulary used in the audio. It can be either Easy, Medium, or Hard.",
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 12),
+                          if (_data.isNotEmpty)
+                            Card(
+                              color: Color.fromARGB(255, 244, 242, 234),
+                              child: Container(
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Relevance between Slide Content & Audio',
+                                      style: TextStyle(
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      relevanceText,
+                                      style: TextStyle(
+                                        color: Colors.black45,
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isRelevanceExpanded =
+                                              !isRelevanceExpanded;
+                                        });
+                                      },
+                                      child: Text(
+                                        isRelevanceExpanded
+                                            ? "See Less"
+                                            : "See More",
+                                        style: TextStyle(
+                                            color: const Color.fromARGB(
+                                                255, 145, 139, 139)),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          SizedBox(height: 12),
+                          if (_data.isNotEmpty)
+                            Card(
+                              color: Color.fromRGBO(5, 38, 57, 1.000),
+                              shape: RoundedRectangleBorder(
+                                side:
+                                    BorderSide(color: Colors.grey, width: 2.0),
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              child: Container(
+                                width: double.infinity,
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Extracted Text from Audio',
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      displayText,
+                                      style: TextStyle(
+                                        color: Colors.white70,
+                                      ),
+                                    ),
+                                    TextButton(
+                                       onPressed: () {
+                                         setState(() {
+                                           isTextExpanded = !isTextExpanded;
+                                         });
+                                       },
+                                       child: Text(
+                                         isTextExpanded
+                                             ? "See Less"
+                                             : "See More",
+                                         style: TextStyle(color: Colors.white),
+                                       ),
+                                     ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                        ],
                       ),
+                    ),
                   ],
                 ),
               ),
