98474a81e554229992192a7ca1acbdf620a2142b
ALIRAZA17 <ialidar2001@gmail.com>
2024-05-02 00:27:31 +0500

    Add emailTextControllerProvider to handle email text editing in login screen

---
+++ lib/screens/login/provider/email_text_controller_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final emailTextControllerProvider =
+    Provider.autoDispose<TextEditingController>((ref) {
+  final controller = TextEditingController();
+  ref.onDispose(() {
+    controller.dispose();
+  });
+  return controller;
+});
