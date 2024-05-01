4afc7496753709947c035d2f56c39856d6266d47
ALIRAZA17 <ialidar2001@gmail.com>
2024-05-01 20:35:36 +0500

    Refactor pptx_to_png.py for better code organization and readability

--- python_server/pptx_to_png.py
+++ python_server/pptx_to_png.py
@@ -8,18 +8,15 @@ import pythoncom
 db = get_firestore_instance()
 bucket = get_storage_bucket()
 
-
 def upload_to_firebase_storage(local_file_path, remote_file_path):
     blob = bucket.blob(remote_file_path)
     blob.upload_from_filename(local_file_path)
     blob.make_public()
     return blob.public_url
 
-
 def update_firestore(user_id, image_urls):
     doc_ref = db.collection("images").document(user_id)
-    doc_ref.set({"imageUrls": firestore.ArrayUnion(image_urls)}, merge=True)
-
+    doc_ref.set({"imageUrls": firestore.ArrayUnion(image_urls)}, merge=False)
 
 def pptx_to_pngs_and_upload(pptx_path, output_dir, user_id):
     if not os.path.exists(output_dir):
@@ -31,14 +28,18 @@ def pptx_to_pngs_and_upload(pptx_path, output_dir, user_id):
     powerpoint.Visible = 1
     try:
         ppt = powerpoint.Presentations.Open(pptx_path)
+        filename = os.path.basename(pptx_path).split('.')[0]
         image_urls = []
 
         for i, slide in enumerate(ppt.Slides):
             image_path = os.path.join(output_dir, f"slide_{i+1}.png")
             slide.Export(image_path, "PNG")
-            remote_path = f"images/slide_{i+1}.png"
+
+            # Define remote path with the filename directory
+            remote_path = f"images/{filename}/slide_{i+1}.png"
             image_url = upload_to_firebase_storage(image_path, remote_path)
             image_urls.append(image_url)
+
             os.remove(image_path)
 
         ppt.Close()
@@ -49,9 +50,8 @@ def pptx_to_pngs_and_upload(pptx_path, output_dir, user_id):
         pythoncom.CoUninitialize()
         shutil.rmtree(output_dir)
 
-
+# Example usage:
 # pptx_path = "D:\projects\FYP\prepvrse\python_server\Lec.pptx"
 # output_dir = "D:\projects\FYP\prepvrse\python_server\\temp"
 # user_id = "LZgH1ZyEn3Q3yy8mEzAINQrNwTN2"
-
 # pptx_to_pngs_and_upload(pptx_path, output_dir, user_id)
