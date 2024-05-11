470be97698d707309a02841f48965db003ce752a
ALIRAZA17 <ialidar2001@gmail.com>
2024-05-11 16:40:29 +0500

    Update API URL in ui_fetch_questions_screen.dart

--- lib/screens/fetched_questions/ui_fetch_questions_screen.dart
+++ lib/screens/fetched_questions/ui_fetch_questions_screen.dart
@@ -35,7 +35,7 @@ class _GeneratedQuestionsScreenState extends State<GeneratedQuestionsScreen> {
       });
       http.Response response = await http.get(
         Uri.parse(
-            'http://10.7.227.148:5000/api/extract?id=$documentId&userId=$userId'),
+            'http://10.7.237.99:5000/api/extract?id=$documentId&userId=$userId'),
       );
       if (response.statusCode == 200) {
         setState(() {
