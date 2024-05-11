b4678d9613cef5ad36e8872d8e0b964a73b7bff5
ALIRAZA17 <ialidar2001@gmail.com>
2024-05-11 17:49:13 +0500

    Update routes.dart to include new screens for starting a session and interview questions

--- lib/common/constants/routes.dart
+++ lib/common/constants/routes.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 import 'package:prepvrse/screens/audio_upload/ui_audio_upload_screen.dart';
 import 'package:prepvrse/screens/feedback/ui_feedback_screen.dart';
+import 'package:prepvrse/screens/start_session/ui_start_session.dart';
 import 'package:prepvrse/screens/start_session/widgets/industry_selection/ui_industry_selection.dart';
 import 'package:prepvrse/screens/login/ui_login_screen.dart';
 import 'package:prepvrse/screens/start_session/widgets/mode_type/ui_mode_type_screen.dart';
 import 'package:prepvrse/screens/signup/ui_signup_screen.dart';
 import 'package:prepvrse/screens/fetched_questions/ui_fetch_questions_screen.dart';
+import 'package:prepvrse/screens/start_session/widgets/mode_type/widgets/interview_questions.dart';
 import 'package:prepvrse/screens/vr_session/ui_vr_session.dart';
 
 appRoutes() => [
@@ -54,6 +56,20 @@ appRoutes() => [
       GetPage(
         name: '/migrate_to_vr',
         page: () => SessionVR(),
+        transition: Transition.fadeIn,
+        transitionDuration: const Duration(milliseconds: 200),
+      ),
+      GetPage(
+        name: '/start_session',
+        page: () => StartSessionScreen(
+          isPresentation: true,
+        ),
+        transition: Transition.fadeIn,
+        transitionDuration: const Duration(milliseconds: 200),
+      ),
+      GetPage(
+        name: '/interview_questions',
+        page: () => InterviewQuestions(),
         transition: Transition.fadeIn,
         transitionDuration: const Duration(milliseconds: 200),
       ),
