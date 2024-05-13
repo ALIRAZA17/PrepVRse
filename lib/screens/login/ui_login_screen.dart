3def2701ffc09ecbdf0b2edb8255beffcbdf7741
ALIRAZA17 <ialidar2001@gmail.com>
2024-05-13 14:01:15 +0500

    chore: Refactor login screen code and add form validation

--- lib/screens/login/ui_login_screen.dart
+++ lib/screens/login/ui_login_screen.dart
@@ -17,6 +17,7 @@ class LoginScreen extends ConsumerStatefulWidget {
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   bool isLoading = false;
+  final _formKey = GlobalKey<FormState>();
   Future<void> signIn(String email, String password) async {
     try {
       setState(() {
@@ -60,98 +61,106 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
         backgroundColor: Styles.primaryColor,
         automaticallyImplyLeading: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 18, bottom: 8, right: 18),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "PrepVRse",
-              style: Styles.displayLargeBoldStyle.copyWith(
-                color: Styles.primaryColor,
-                fontSize: 32,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            AppTextField(
-              label: "Email",
-              keyboardType: TextInputType.name,
-              controller: emailController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Email is required';
-                } else if (!RegExp(
-                        r'^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})')
-                    .hasMatch(
-                  value,
-                )) {
-                  return "Enter a valid email address";
-                }
-                return null;
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            AppTextField(
-              label: "Password",
-              keyboardType: TextInputType.name,
-              controller: passwordController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Password is required';
-                }
-                return null;
-              },
-              obscureText: true,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : AppTextButton(
-                    text: "Login",
-                    onTap: () async {
-                      final email = ref.read(emailTextControllerProvider).text;
-                      final password =
-                          ref.read(passwordTextControllerProvider).text;
-                      await signIn(email, password);
-                    },
-                    color: Styles.primaryColor,
-                  ),
-            const SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-              onTap: () {
-                Get.toNamed('/signup');
-              },
-              child: RichText(
-                text: TextSpan(
-                  text: "Don't have an account? ",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Sign Up',
-                      style: TextStyle(
-                        color: Styles.primaryColor,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 18, bottom: 8, right: 18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "PrepVRse",
+                style: Styles.displayLargeBoldStyle.copyWith(
+                  color: Styles.primaryColor,
+                  fontSize: 32,
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              AppTextField(
+                key: ValueKey('emailTextField'),
+                label: "Email",
+                keyboardType: TextInputType.name,
+                controller: emailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email is required';
+                  } else if (!RegExp(
+                          r'^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})')
+                      .hasMatch(
+                    value,
+                  )) {
+                    return "Enter a valid email address";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              AppTextField(
+                key: ValueKey('passwordTextField'),
+                label: "Password",
+                keyboardType: TextInputType.name,
+                controller: passwordController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Password is required';
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : AppTextButton(
+                      text: "Login",
+                      onTap: () async {
+                        if (_formKey.currentState!.validate()) {
+                          final email =
+                              ref.read(emailTextControllerProvider).text;
+                          final password =
+                              ref.read(passwordTextControllerProvider).text;
+                          await signIn(email, password);
+                        }
+                      },
+                      color: Styles.primaryColor,
+                    ),
+              const SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.toNamed('/signup');
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Don't have an account? ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Sign Up',
+                        style: TextStyle(
+                          color: Styles.primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
